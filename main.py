6a1127ea0ad1100c967b08d0f8fa0fe36f936f96
Kian-o <oreillym151@gmail.com>
2022-07-17 21:12:38 -0600

    day4
    day4

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-print('''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
-|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
-direction1 = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right' ").lower()
-if direction1 == "right":
-    print("You fell into a hole. Game Over.")
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+options = [rock, paper, scissors]
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+print(options[choice])
+
+computer_choice = random.randint(0,2)
+print(f"Computer chooses:\n {options[computer_choice]}")
+
+if choice == 0 and computer_choice == 2:
+  print("You Win")
+elif choice == 1 and computer_choice == 0:
+  print("You Win")
+elif choice == 2 and computer_choice == 1:
+  print("You Win")
+elif choice == computer_choice:
+  print("Its a draw!")
 else:
-    direction2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ").lower()
-if direction2 == "swim":
-    print("Attacked by trout. Game Over.")
-else:
-    door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose? ").lower()
-if door == "red":
-    print("Burned by fire. Game Over.")
-elif door == "yellow":
-    print("You Win!")
-elif door == "blue":
-    print("Eaten by beasts. Game Over.")
-else:
-    print("Game Over.")
+  print("You Lose")
+
