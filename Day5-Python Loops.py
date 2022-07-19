b4c742c36e1d73d4b3b96da3a5950a821f8f0fe5
Kian-o <oreillym151@gmail.com>
2022-07-19 08:02:57 -0600

    day5 updates
    day5 updates

--- Day5-Python Loops.py
+++ Day5-Python Loops.py
@@ -1,5 +1,56 @@
 #######################Day 5 Final Project: Password Generator################
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Easy mode - order not randomized#
+let = random.sample(letters, k=nr_letters)
+sym = random.sample(symbols, k=nr_symbols)
+num = random.sample(numbers, k=nr_numbers)
+test = let + sym + num
+easypw = ""
+for i in test:
+  easypw += i
+print(f"Here is the easy password: {easypw}")
+#OR#
+password = ""
+for char in range(1, nr_letters +1):
+  password += random.choice(letters)
+for char in range(1, nr_symbols +1):
+  password += random.choice(letters)
+for char in range(1, nr_numbers +1):
+  password += random.choice(numbers)
+print(password)
+
+#Hard mode - order randomized#
+let = random.sample(letters, k=nr_letters)
+sym = random.sample(symbols, k=nr_symbols)
+num = random.sample(numbers, k=nr_numbers)
+test2 = let + sym + num
+random.shuffle(test2)
+hardpw = ""
+for i in test2:
+  hardpw += i
+print(f"Here is the hard password: {hardpw}")
+#OR#
+password = []
+for char in range(1, nr_letters +1):
+  password.append(random.choice(letters))
+for char in range(1, nr_symbols +1):
+  password.append(random.choice(symbols))
+for char in range(1, nr_numbers +1):
+  password.append(random.choice(symbols))
+random.shuffle(password)
+pw = ""
+for char in password:
+  pw += char
+print(pw)
 
 
 
@@ -65,3 +116,14 @@ total = 0
 for number in range(2, 101, 2):
   total += number
 print(number)
+
+#FixxBuzz Job Interview#
+for number in range (1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("Fizz Buzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
