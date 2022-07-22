5a40db9a2a2e68d50044ef5dd71514baaca807ca
Kian-o <oreillym151@gmail.com>
2022-07-21 18:03:38 -0600

    hangman!

---
+++ Day7-Beginner - Hangman.py
@@ -0,0 +1,158 @@
+#######################Day 7 Project: Hangman######################
+#Final thing#
+import random
+import hangman_words
+import hangman_art
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(hangman_words.word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+word_length = len(chosen_word)
+lives = 6
+end_of_game = False
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+print(hangman_art.logo)
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
+
+  for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+  if guess not in chosen_word:
+    lives -= 1
+    if lives == 0:
+      end_of_game = True
+      print("You Lose")
+  print(f"{' '.join(display)}")
+  print(hangman_art.stages[lives])
+  if "_" not in display:
+    end_of_game = True
+    print("You Win!!")
+
+#Hangman part I#
+word_list = ["aardvark", "baboon", "camel"]
+import random
+choose_word = random.choice(word_list)
+
+guess = input("Choose a letter: ").lower()
+
+for letter in choose_word:
+  if letter == guess:
+    print("correct")
+  else:
+    print("wrong")
+
+#Hangman part II#
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+  display += "_"
+print(display)
+
+guess = input("Guess a letter: ").lower()
+
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+print(display)
+
+#Hangman part III#
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+word_length = len(chosen_word)
+lives = 6
+end_of_game = False
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
+
+  for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+  if guess not in chosen_word:
+    lives -= 1
+    if lives == 0:
+      end_of_game = True
+      print("You Lose")
+  print(f"{' '.join(display)}")
+  print(stages[lives])
+  if "_" not in display:
+    end_of_game = True
+    print("You Win!!")
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-total = 0
-for number in range(1, 101):
-  if number % 2 == 0:
-    total += number
-    print(total)
+import random
+import hangman_words
+import hangman_art
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(hangman_words.word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+word_length = len(chosen_word)
+lives = 6
+end_of_game = False
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+print(hangman_art.logo)
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
+
+  for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+  if guess not in chosen_word:
+    lives -= 1
+    if lives == 0:
+      end_of_game = True
+      print("You Lose")
+  print(f"{' '.join(display)}")
+  print(hangman_art.stages[lives])
+  if "_" not in display:
+    end_of_game = True
+    print("You Win!!")
