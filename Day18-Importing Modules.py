9763179a6be5584d710cc6a8c33ffd8af844b8a0
Kian-o <oreillym151@gmail.com>
2022-08-29 20:21:17 -0600

    turtle_hirst

--- Day18-Importing Modules.py
+++ Day18-Importing Modules.py
@@ -1,6 +1,58 @@
 from turtle import Turtle, Screen
+import colorgram
 import turtle as t
 import random
+
+color_list = [(212, 154, 97), (239, 246, 243), (52, 108, 132), (178, 78, 33), (198, 143, 34), (123, 80, 97),
+              (235, 240, 244), (116, 155, 171), (124, 175, 158), (228, 197, 129), (194, 85, 105), (54, 38, 20),
+              (12, 51, 65), (189, 123, 142), (54, 120, 115), (41, 169, 126), (167, 21, 31), (225, 94, 78), (4, 30, 28),
+              (39, 32, 34), (243, 163, 159), (81, 148, 168), (164, 27, 22), (239, 163, 167), (104, 123, 159),
+              (164, 209, 193), (21, 81, 93), (29, 85, 81)]
+
+turtle_hirst = t.Turtle()
+turtle_hirst.shape("turtle")
+turtle_hirst.penup()
+turtle_hirst.speed("fastest")
+turtle_hirst.pensize(20)
+t.colormode(255)
+turtle_hirst.setheading(255)
+turtle_hirst.forward(300)
+turtle_hirst.setheading(0)
+number_of_dots = 100
+
+for _ in range(1, number_of_dots + 1):
+    turtle_hirst.dot(20, random.choice(color_list))
+    turtle_hirst.forward(50)
+
+    if _ % 10 == 0:
+        turtle_hirst.setheading(90)
+        turtle_hirst.forward(50)
+        turtle_hirst.setheading(180)
+        turtle_hirst.forward(500)
+        turtle_hirst.setheading(0)
+
+
+screen = t.Screen()
+screen.exitonclick()
+
+
+
+
+
+hirst_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    hirst_colors.append(new_color)
+print(hirst_colors)
+
+color_list = [(212, 154, 97), (239, 246, 243), (52, 108, 132), (178, 78, 33), (198, 143, 34), (123, 80, 97), (235, 240, 244), (116, 155, 171), (124, 175, 158), (228, 197, 129), (194, 85, 105), (54, 38, 20), (12, 51, 65), (189, 123, 142), (54, 120, 115), (41, 169, 126), (167, 21, 31), (225, 94, 78), (4, 30, 28), (39, 32, 34), (243, 163, 159), (81, 148, 168), (164, 27, 22), (239, 163, 167), (104, 123, 159), (164, 209, 193), (21, 81, 93), (29, 85, 81)]
+
+
+
 
 """Million Dollar Painting!"""
 
--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import turtle as t
 import random
 
-"""Python Turtle"""
-spiro = t.Turtle()
+color_list = [(212, 154, 97), (239, 246, 243), (52, 108, 132), (178, 78, 33), (198, 143, 34), (123, 80, 97),
+              (235, 240, 244), (116, 155, 171), (124, 175, 158), (228, 197, 129), (194, 85, 105), (54, 38, 20),
+              (12, 51, 65), (189, 123, 142), (54, 120, 115), (41, 169, 126), (167, 21, 31), (225, 94, 78), (4, 30, 28),
+              (39, 32, 34), (243, 163, 159), (81, 148, 168), (164, 27, 22), (239, 163, 167), (104, 123, 159),
+              (164, 209, 193), (21, 81, 93), (29, 85, 81)]
+
+turtle_hirst = t.Turtle()
+turtle_hirst.shape("turtle")
+turtle_hirst.penup()
+turtle_hirst.speed("fastest")
+turtle_hirst.pensize(20)
 t.colormode(255)
-spiro.shape("turtle")
-spiro.speed('fastest')
+turtle_hirst.setheading(255)
+turtle_hirst.forward(300)
+turtle_hirst.setheading(0)
+number_of_dots = 100
+
+for _ in range(1, number_of_dots + 1):
+    turtle_hirst.dot(20, random.choice(color_list))
+    turtle_hirst.forward(50)
+
+    if _ % 10 == 0:
+        turtle_hirst.setheading(90)
+        turtle_hirst.forward(50)
+        turtle_hirst.setheading(180)
+        turtle_hirst.forward(500)
+        turtle_hirst.setheading(0)
 
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    return r, g, b
-
-
-this_list_this_list = [0, 90, 100, 270]
-
-
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        spiro.pencolor(random_color())
-        spiro.setheading(spiro.heading() + size_of_gap)
-        spiro.circle(100)
-
-
-draw_spirograph(5)
 screen = t.Screen()
 screen.exitonclick()
