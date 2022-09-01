3417fa1caea71d536ff330ef95e72a5efd8a4204
Kian-o <oreillym151@gmail.com>
2022-08-31 18:59:48 -0600

    off to the races!!

--- Day 19-Instances, States, and Higher Order Functions.py
+++ Day 19-Instances, States, and Higher Order Functions.py
@@ -1,4 +1,3 @@
-import turtle
 from turtle import Turtle, Screen
 import random
 
@@ -35,3 +34,43 @@ while is_race_on:
 
 
 screen.exitonclick()
+"""Etching and Sketching:"""
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def counter_clockwise():
+    tim.left(45)
+
+
+def clockwise():
+    tim.right(45)
+
+
+def erase():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+"""Higher order functions are functions that can work with other functions"""
+"""Object State and Instances (different instances can have different states)"""
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=counter_clockwise)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="c", fun=erase)
+screen.exitonclick()
+
+"""Event Listeners"""
