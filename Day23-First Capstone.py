d7f19e1d3cdd8f73cf97f623d550c0ccb591eb8e
Kian-o <oreillym151@gmail.com>
2022-09-29 12:40:27 -0600

    First Capstone finished

--- Day23-First Capstone.py
+++ Day23-First Capstone.py
@@ -2,22 +2,36 @@ import time
 from turtle import Screen
 from turtle_crossing import Player, Scoreboard, CarManager
 
+FINISH_LINE_Y = 280
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.title("Turtle Crossing!")
 player = Player()
 scoreboard = Scoreboard()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(fun=player.move, key="Up")
-
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move()
 
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
 
+    if player.is_at_finish_line():
+        scoreboard.next_level()
+        scoreboard.refresh()
+        player.go_to_start()
+        car_manager.level_up()
+
+screen.exitonclick()
 
