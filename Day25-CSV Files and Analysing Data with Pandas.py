839eefd26dedd37b1ebef01fc5532a67b2fba3a2
Michael <oreillym151@gmail.com>
2022-10-06 14:28:15 -0600

    Working with .CSV files and Pandas

--- Day25-CSV Files and Analysing Data with Pandas.py
+++ Day25-CSV Files and Analysing Data with Pandas.py
@@ -1,16 +1,22 @@
-# import csv
-#
-#
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != 'temp':
-#             temperatures.append(int(row[1]))
-#
-# print(temperatures)
+import csv
 import pandas
 import numpy as np
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != 'temp':
+            temperatures.append(int(row[1]))
+
+print(temperatures)
+
+# Or
+
+data = pandas.read_csv('weather_data.csv')
+print(data['temp'])
+
+# Or
 
 data = pandas.read_csv('weather_data.csv')
 data_dict = data.to_dict()
@@ -25,5 +31,4 @@ print(data['temp'].mean())
 print(np.mean(temp_list))
 
 print(data['temp'].max())
-
 
